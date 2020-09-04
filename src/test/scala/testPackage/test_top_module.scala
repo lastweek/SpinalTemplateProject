3c5beeb295bfedcfb15d55ad3cf3ce6e0867711c
lastweek <syzwhat@gmail.com>
2020-09-03 20:50:54 -0700

    Update sim bench and move to linux

--- src/test/scala/testPackage/test_top_module.scala
+++ src/test/scala/testPackage/test_top_module.scala
@@ -1,9 +1,19 @@
+package testPackage
+
 import org.scalatest._
+
 import spinal.core._
-import spinal.lib._
-import spinal.lib.fsm._
-import spinal.lib.bus.amba4.axi._
+import spinal.core.sim._
 
 class test_top_module extends FunSuite {
-  println("asd");
+    println("OUt");
+
+    SimConfig.withWave.compile(new testStateMachineTop).doSim { dut =>
+      dut.clockDomain.forkStimulus(10)
+
+      for (i <- 0 to 3) {
+        dut.clockDomain.waitSampling()
+        println(dut.io.result)
+      }
+    }
 }
